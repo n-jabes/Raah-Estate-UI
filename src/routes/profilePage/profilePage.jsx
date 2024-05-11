dc64ad706e76b64f98fd0bb151bb0ef1c4c2745d
n-jabes <nshutij7@gmail.com>
2024-05-11 12:03:02 +0200

    integrated authentication

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,8 +1,21 @@
-import Chat from "../../components/chat/Chat";
-import List from "../../components/list/List";
-import "./profilePage.scss";
+import { useNavigate } from 'react-router-dom';
+import Chat from '../../components/chat/Chat';
+import List from '../../components/list/List';
+import './profilePage.scss';
+import apiRequest from '../../lib/apiRequest';
 
 function ProfilePage() {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      const res = await apiRequest.post('/auth/logout');
+      localStorage.removeItem('user');
+      navigate('/');
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="profilePage">
       <div className="details">
@@ -25,6 +38,7 @@ function ProfilePage() {
             <span>
               E-mail: <b>john@gmail.com</b>
             </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
@@ -39,7 +53,7 @@ function ProfilePage() {
       </div>
       <div className="chatContainer">
         <div className="wrapper">
-          <Chat/>
+          <Chat />
         </div>
       </div>
     </div>
